446858463c775c5d8472111093e2571232206ffe
ftcardoso <franciscotcardoso@hotmail.com>
2016-12-22 14:43:24 +0000

    Added embers_client script and constants file
    embers_client: script to subscribe a gateway that will receive messages sent by HTTP, CoAP and MQTT
    constants: file to configure the host, port and the gateway credentials

---
+++ constants.py
@@ -0,0 +1,8 @@
+
+HOST = 'broker.embers.citibrain.com'
+PORT = '80'
+
+CREDENTIALS = {
+    'uuid': '6831f228-0ac6-4940-ac34-443d8c181c9a',
+    'token': 'c1b410f8262b2e654737e59bb5248d19a993583c',
+}
